39277b6cc1eddb561fccb47aa29129d116ab3c6a
Steve <wsfuller.dev@gmail.com>
2021-07-18 19:07:46 -0700

    Updating Footer
    - Adding WSF build badge images
    - Adding TMDB logo
    - Adding WSF Logo
    - Updating Footer

--- src/components/AppFooter/AppFooter.tsx
+++ src/components/AppFooter/AppFooter.tsx
@@ -1,15 +1,54 @@
 import React from 'react';
 
-import { Stack } from '@fluentui/react';
+import { useTheme, Icon, Link, Stack, Text } from '@fluentui/react';
 
 import useFooterStyles from './AppFooter.styles';
 
+import WSFBuildBadgeDark from '../../assets/images/wsf-build-badge-dark.svg';
+import WSFBuildBadgeLight from '../../assets/images/wsf-build-badge-light.svg';
+import theMovieDatabaseLogo from '../../assets/images/the-movie-database-logo.svg';
+
+import { useAppSelector, useBreakpoints, useWindowSize } from '../../utils/hooks';
+
 const AppFooter: React.FC = () => {
   const classes = useFooterStyles();
+  const {
+    appTheme: { isDarkMode },
+  } = useAppSelector((state) => state);
+  const theme = useTheme();
+  const size = useWindowSize();
+  const breakpoints = useBreakpoints();
+  const currentYear = new Date().getFullYear();
+
+  const isFooterContentStackHorizontal = size.width >= breakpoints.large;
+
+  const wsfBuildBadge = isDarkMode ? WSFBuildBadgeLight : WSFBuildBadgeDark;
 
   return (
-    <Stack as="footer" className={classes.root} horizontal verticalAlign="center">
-      <h1>App footer</h1>
+    <Stack as="footer" className={classes.root}>
+      <Stack
+        className="grid-container"
+        horizontal={isFooterContentStackHorizontal}
+        horizontalAlign={isFooterContentStackHorizontal ? 'space-between' : 'center'}
+        verticalAlign="center"
+        tokens={{ childrenGap: theme.spacing.m }}
+      >
+        <Stack.Item>
+          <Link href="https://wsfuller.dev" className={classes.wsfBrandingLink} target="_blank" rel="noopener">
+            <img className="image-responsive" src={wsfBuildBadge} alt="WSF Logo" />
+          </Link>
+        </Stack.Item>
+        <Stack.Item>
+          <Text variant="mediumPlus">
+            Made with <Icon className={classes.copyRightIcon} iconName="HeartFill" /> {currentYear}
+          </Text>
+        </Stack.Item>
+        <Stack.Item>
+          <Link href="https://www.themoviedb.org/" className={classes.tmdbLogoLink} target="_blank" rel="noopener">
+            <img className="image-responsive" src={theMovieDatabaseLogo} alt="The Movie Database Logo" />
+          </Link>
+        </Stack.Item>
+      </Stack>
     </Stack>
   );
 };
