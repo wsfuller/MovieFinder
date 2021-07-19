1163dc14be07e5416fb2626f9108714e984a03c0
Steve Fuller <wsfuller.dev@gmail.com>
2021-07-18 19:47:04 -0700

    Task/footer (#16)
    * Updating Footer
    
    - Adding WSF build badge images
    - Adding TMDB logo
    - Adding WSF Logo
    - Updating Footer
    
    * Resize TMDB logo
    
    Making the logo smaller by 30%

--- src/components/AppFooter/AppFooter.styles.ts
+++ src/components/AppFooter/AppFooter.styles.ts
@@ -4,8 +4,28 @@ const useAppFooterStyles = makeStyles((theme) => {
   return {
     root: {
       width: '100%',
-      padding: `${theme.spacing.s1} ${theme.spacing.m}`,
+      padding: `${theme.spacing.l2} ${theme.spacing.m}`,
       backgroundColor: theme.palette.neutralLight,
+    },
+    wsfBrandingLink: {
+      display: 'block',
+      width: '100%',
+      maxWidth: 180,
+      height: 'auto',
+      opacity: 0.8,
+      transition: 'opacity .25s ease-in-out',
+      selectors: {
+        '&:hover': {
+          opacity: 1,
+        },
+      },
+    },
+    copyRightIcon: {
+      color: theme.palette.red,
+    },
+    tmdbLogoLink: {
+      display: 'block',
+      maxWidth: 60,
     },
   };
 });
