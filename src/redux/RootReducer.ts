e070f64be2386546a380458206a04b15a0c514ed
Steve <wsfuller.dev@gmail.com>
2021-07-27 17:25:42 -0700

    Instantiate search reducer

--- src/redux/RootReducer.ts
+++ src/redux/RootReducer.ts
@@ -3,11 +3,13 @@ import { combineReducers } from 'redux';
 import appThemeReducer from './appTheme/appThemeReducer';
 import moviesReducer from './movies/moviesReducer';
 import panelsReducer from './panels/panelsReducer';
+import searchReducer from './search/searchReducer';
 
 const RootReducer = combineReducers({
   appTheme: appThemeReducer,
   movies: moviesReducer,
   panels: panelsReducer,
+  searchResults: searchReducer,
 });
 
 export default RootReducer;
